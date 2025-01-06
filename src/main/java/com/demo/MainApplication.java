481e0d9d2f66c92a3edfcc2e6cfb34242ac02230
sherrywu <hsuan.wu9211@gmail.com>
2025-01-06 10:24:57 +0800

    1. 完成websocket連線，處理連線成功與失敗結果 2. 增加websocket相關log"

--- src/main/java/com/demo/MainApplication.java
+++ src/main/java/com/demo/MainApplication.java
@@ -1,43 +1,34 @@
 package com.demo;
 
+import com.demo.controller.WebsocketController;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
-
-@SpringBootApplication
 public class MainApplication extends Application {
 
 	private ConfigurableApplicationContext springContext;
+	private WebsocketController websocketController = new WebsocketController();
 
 	@Override
 	public void init() throws Exception {
-		springContext = new SpringApplicationBuilder(MainApplication.class).run();
+		springContext = new SpringApplicationBuilder(Main.class).run();
 	}
 
 	@Override
 	public void stop() throws Exception {
-		springContext.close(); 	// 關閉 Spring 上下文
+		springContext.close();
 	}
-
-	public static void main(String[] args) {
-//		SpringApplication.run(MainApplication.class, args);
-		launch();
-		System.out.println("-------B專案啟動-------");
-	}
-
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/static/view/webSocketView.fxml"));
-		Scene scene = new Scene(fxmlLoader.load(), 600, 600);
-		stage.setTitle("連線系統");
-		stage.setScene(scene);
-		stage.show();
+		// 顯示主畫面
+		websocketController.showView(stage);
 	}
+
+	public static void main(String[] args) {
+		launch();
+	}
+
 }
